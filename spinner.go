135d480f7be846c662bc4fd61a1ab3015af3c977
ryanshu <876324872@qq.com>
2021-07-09 01:27:55 +0800

    spinner.go with only main goroutine

---
+++ spinner.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fib2(n) // slow
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib2(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib2(x-1) + fib2(x-2)
+}
