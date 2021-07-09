f41cbdc170d77cebeeb62aa6ba4f9a2ff222ac9a
ryanshu <876324872@qq.com>
2021-07-10 02:53:00 +0800

    makeImageFile.go with channels

---
+++ thumbnail/thumbnail/makeImageFile.go
@@ -0,0 +1,8 @@
+package thumbnail
+
+// ImageFile reads an image from infile and writes
+// a thumbnail-size version of it in the same directory.
+// It returns the generated file name, e.g., "foo.thumb.jpg".
+func ImageFile(infile string) (string, error) {
+	return "", nil
+}
